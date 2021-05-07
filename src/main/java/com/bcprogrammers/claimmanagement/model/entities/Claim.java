30ab50c106952aba25ab4a056af4b76b24881db7
keyla cusi <keylqsy@gmail.com>
2021-05-07 14:56:14 -0500

    save claim

--- src/main/java/com/bcprogrammers/claimmanagement/model/entities/Claim.java
+++ src/main/java/com/bcprogrammers/claimmanagement/model/entities/Claim.java
@@ -18,29 +18,28 @@ public class Claim {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "CLAIM_ID")
+    @JoinColumn(name = "CLAIM_ID")
     private Long claimId;
 
     @ManyToOne
-    @Column(name = "USER_ID")
     @JoinColumn(name = "USER_ID")
     private User user;
 
-    @Column(name = "DESCRIPTION")
+    @JoinColumn(name = "DESCRIPTION")
     private String description;
 
-    @Column(name = "CLAIM_TYPE")
+    @JoinColumn(name = "CLAIM_TYPE")
     private Integer claimType;
 
-    @Column(name = "CREATE_DATE")
+    @JoinColumn(name = "CREATE_DATE")
     private LocalDateTime createDate;
 
-    @Column(name = "LAST_UPDATED_DATE")
+    @JoinColumn(name = "LAST_UPDATED_DATE")
     private LocalDateTime lastUpdatedDate;
 
-    @Column(name = "EXPECTED_DATE_ATTENTION")
+    @JoinColumn(name = "EXPECTED_DATE_ATTENTION")
     private LocalDateTime expectedDateAttention;
 
-    @Column(name = "LAST_UPDATED_BY")
+    @JoinColumn(name = "LAST_UPDATED_BY")
     private String lastUpdatedBy;
 }
